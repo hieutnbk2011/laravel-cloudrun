6b8a8c335a2dc7446eefcfbc1e6754e9f3aa83bb
hieu <hieu.tnbk2011@yahoo.com>
2021-02-18 03:17:21 +0000

    changed to apache

--- config/cmd.sh
+++ config/cmd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd /var/www/html 
-runuser -u www-data -- php artisan migrate --force --no-interaction
+#Migrate Command
+#cd /var/www/html 
+#runuser -u www-data -- php artisan migrate --force --no-interaction
 # Start apache
 /usr/sbin/apache2ctl -DFOREGROUND
